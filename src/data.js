fa95f2824be992e1104b8aa983f693748fdd58ca
DT Hang <dthang@example.com>
2026-07-28 16:58:02 +0700

    Clean up local videos to only use Cloudinary

--- src/data.js
+++ src/data.js
@@ -1,16 +1,5 @@
 export const reelsData = [
   {
-    id: 9,
-    title: "",
-    pinyin: "",
-    vietnamese: "",
-    image: "", 
-    videoUrl: "videos/video_9.mp4",
-    views: "1,2K",
-    likes: "500"
-  }
-,
-  {
     id: 10,
     title: "",
     pinyin: "",
